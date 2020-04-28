cb4b07e89a58f0d3e315de13ce388524da776330
niuhb2 <niuhb2@lenovo.com>
2020-04-28 15:08:45 +0800

    汉罗塔算法

---
+++ 01 汉罗塔/01 hanio.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 开发人员   ：Davis Niu
+# 开发时间   ：4/28/2020  02:57 PM 
+# 文件名称   ：01 hanio.PY
+# 开发工具   ：PyCharm
+
+
+def hanio(n, a, b, c):
+    """
+    递归实现汉罗塔算法
+    :param n: 圆盘数量
+    :param a: 柱子
+    :param b: 柱子
+    :param c: 柱子
+    :return: None
+    """
+    if n > 0:
+        hanio(n-1, a, c, b)
+        print('move from %s to %s' % (a, c))
+        hanio(n-1, b, a, c)
+
+
+hanio(3, 'A', 'B', 'C')
