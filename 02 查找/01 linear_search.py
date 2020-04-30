0eb139ee2749f327f54a8012437f0fb1fe7c620f
niuhb2 <niuhb2@lenovo.com>
2020-04-30 15:44:47 +0800

    更新线性查找算法

--- 02 查找/01 linear_search.py
+++ 02 查找/01 linear_search.py
@@ -4,8 +4,12 @@
 # 开发时间   ：4/28/2020  03:21 PM 
 # 文件名称   ：01 linear_search.PY
 # 开发工具   ：PyCharm
+import cal_time
+import print_log
 
 
+@cal_time.cal_time
+@print_log.print_log
 def linear_search(li, val):
     """
     顺序查找算法
@@ -18,3 +22,6 @@ def linear_search(li, val):
             return index
     else:
         return None
+
+
+print(linear_search(list(range(10000000)), 9999999))
